2b77ac5175432c8abf9edc6c7ce5c81c15b58323
junnaaa <paxtonbennison@outlook.com>
2025-06-15 18:36:59 +0800

    final commit before new project

--- kys/hello_world.c
+++ kys/hello_world.c
@@ -1,12 +1,13 @@
 #include "pico/stdlib.h"
+#include "pico/time.h"
 #include "hardware/uart.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-#define IN1 10
-#define IN2 11
-#define IN3 12
-#define IN4 13
+#define IN1 0
+#define IN2 1
+#define IN3 6
+#define IN4 7
 
 const uint8_t STEPS[4] = {
   0b0001,
@@ -28,7 +29,7 @@ void move_stepper(int steps, int step_delay) {
     gpio_put(IN3, STEPS[phase] & 4);  
     gpio_put(IN4, STEPS[phase] & 8);  
 
-    sleep_ms(step_delay);
+    busy_wait_us(step_delay);
     
   }
   gpio_put(IN1, 0); gpio_put(IN2, 0);  
@@ -48,9 +49,9 @@ int main() {
   gpio_put(IN3, 1); sleep_ms(300); gpio_put(IN3, 0);  
   gpio_put(IN4, 1); sleep_ms(300); gpio_put(IN4, 0);  
   while(1) {  
-        move_stepper(2048, 2);  // 1 full rotation forward  
+        move_stepper(2048, 2000);  // 1 full rotation forward  
         sleep_ms(1000);         // rest 1 sec  
-        move_stepper(-2048, 2); // 1 rotation backward  
+        move_stepper(-2048, 2000); // 1 rotation backward  
         sleep_ms(1000);  
     }  
 }
