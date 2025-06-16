aeaa7b66e362253bf8c2651893f7651dc4db3296
junnnaaaaaa <paxtonbennison@outlook.com>
2025-06-16 17:04:52 +0800

    everything

--- kys/build/pico-sdk/cmake_install.cmake
+++ kys/build/pico-sdk/cmake_install.cmake
@@ -39,22 +39,22 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/junnaaa/project/kys/build/pico-sdk/tools/cmake_install.cmake")
+  include("/home/junnaaa/cubebot-project/kys/build/pico-sdk/tools/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/junnaaa/project/kys/build/pico-sdk/src/cmake_install.cmake")
+  include("/home/junnaaa/cubebot-project/kys/build/pico-sdk/src/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/junnaaa/project/kys/build/pico-sdk/docs/cmake_install.cmake")
+  include("/home/junnaaa/cubebot-project/kys/build/pico-sdk/docs/cmake_install.cmake")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/junnaaa/project/kys/build/pico-sdk/install_local_manifest.txt"
+  file(WRITE "/home/junnaaa/cubebot-project/kys/build/pico-sdk/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- kys/build/pico-sdk/src/rp2_common/pico_cyw43_driver/cmake_install.cmake
+++ kys/build/pico-sdk/src/rp2_common/pico_cyw43_driver/cmake_install.cmake
@@ -39,12 +39,12 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/junnaaa/project/kys/build/pico-sdk/src/rp2_common/pico_cyw43_driver/cybt_shared_bus/cmake_install.cmake")
+  include("/home/junnaaa/cubebot-project/kys/build/pico-sdk/src/rp2_common/pico_cyw43_driver/cybt_shared_bus/cmake_install.cmake")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/junnaaa/project/kys/build/pico-sdk/src/rp2_common/pico_cyw43_driver/install_local_manifest.txt"
+  file(WRITE "/home/junnaaa/cubebot-project/kys/build/pico-sdk/src/rp2_common/pico_cyw43_driver/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
